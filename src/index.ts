259657125dc87798602bb9ea96a237673a99578c
Manuel Silenzi <manuelsilenzi1@gmail.com>
2021-12-31 13:25:34 -0300

    basic CRUD operations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
+import db from './firebase/config'
+
+const user = {
+  name: 'Manuel',
+  active: true,
+  birthday: 0,
+}
 
 
+const id = 'G3da7WNR9xfCfvzQ4q74'  // id de un documento en users
 
-const mensaje = 'Hola Mundo!';
+// AGREGAR INFORMACIÓN
+// db.collection('users').add(user).then(console.log)
 
-console.log( mensaje );
+// ACTUALIZAR INFORMACIÓN
+// db.collection('users').doc(id).update({ active: true, age: 20 })
+// db.collection('users').doc(id).set({ name: 'Pedro' })
+
+// ELIMINAR UN DOCUMENTO
+// db.collection('users').doc(id).delete()
